5eb36b8c8db02d37f5c43f9c8003d9abe4cb8c35
Nataliadiez <natalia.diez10@hotmail.com>
2026-05-31 20:34:13 -0300

    test de login

--- tests/test_login.py
+++ tests/test_login.py
@@ -8,3 +8,36 @@ Criterios mínimos:
 Login automatizado con espera explícita y validación de /inventory.html y “Products/Swag Labs”. 
 """
 
+from utils.saucedemo_funciones import (
+    setup_driver,
+    login_saucedemo,
+    validar_login_exitoso,
+    USUARIO_VALIDO,
+    PASSWORD_VALIDO
+)
+
+
+def test_login_exitoso():
+    """Prueba el inicio de sesión exitoso en SauceDemo."""
+
+    # Inicializar el driver
+    driver = setup_driver()
+
+    try:
+        # Ejecutar login con credenciales válidas
+        login_saucedemo(
+            driver,
+            USUARIO_VALIDO,
+            PASSWORD_VALIDO
+        )
+
+        # Validar que el login fue exitoso
+        titulo = validar_login_exitoso(driver)
+
+        print("Login exitoso")
+        print("Título de sección OK: ", titulo)
+
+    finally:
+        # Cerrar el navegador al finalizar la prueba
+        print("Cerrando el navegador.")
+        driver.quit()
