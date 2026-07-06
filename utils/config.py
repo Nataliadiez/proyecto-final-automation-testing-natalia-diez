3ddc385d0f897330be61d610ebcd09e30564b753
Nataliadiez <natalia.diez10@hotmail.com>
2026-07-06 16:52:38 -0300

    reestructuración de la pre entrega

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,7 @@
-# Datos base del sitio
-URL_LOGIN = "https://www.saucedemo.com/"
+BASE_URL = "https://www.saucedemo.com"
+LOGIN_URL = f"{BASE_URL}/"
+INVENTORY_URL = f"{BASE_URL}/inventory.html"
+
+# Credenciales válidas provistas por SauceDemo para prácticas.
 USUARIO_VALIDO = "standard_user"
 PASSWORD_VALIDO = "secret_sauce"
